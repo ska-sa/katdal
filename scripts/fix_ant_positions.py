8e3fc562a71d14e5c1966340158bec90c1042d33
Ludwig Schwardt <ludwig@ska.ac.za>
2011-09-26 12:00:02 +0200

    Updated antenna locations as provided by Stephen Muchovej, produced by applying his source-subtraction algorithm to 1314375234.h5, a six-hour baseline cal run taken on 2011-08-26. The accuracy should be on the order of millimetres, and according to Stephen there might still be some (small) residual issues with antennas 2 and 7.
    git-svn-id: https://katfs.kat.ac.za/svnDS/code/katfile/trunk@11096 b56c7a7d-ac47-0410-a905-b6f7de78896a

--- scripts/fix_ant_positions.py
+++ scripts/fix_ant_positions.py
@@ -11,15 +11,14 @@ if len(sys.argv) < 2:
 
 f = h5py.File(sys.argv[1])
 
-f['MetaData/Configuration/Antennas/ant1'].attrs['description'] = 'ant1, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, 25.095 -9.095 0.045, , 1.22'
-f['MetaData/Configuration/Antennas/ant2'].attrs['description'] = 'ant2, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, 90.288 26.389 -0.238, , 1.22'
-f['MetaData/Configuration/Antennas/ant3'].attrs['description'] = 'ant3, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, 3.985 26.899 -0.012, , 1.22'
-f['MetaData/Configuration/Antennas/ant4'].attrs['description'] = 'ant4, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, -21.600 25.500 0.000, , 1.22'
-f['MetaData/Configuration/Antennas/ant5'].attrs['description'] = 'ant5, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, -38.264 -2.586 0.371, , 1.22'
-f['MetaData/Configuration/Antennas/ant6'].attrs['description'] = 'ant6, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, -61.580 -79.685 0.690, , 1.22'
-f['MetaData/Configuration/Antennas/ant7'].attrs['description'] = 'ant7, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, -87.979 75.756 0.125, , 1.22'
+f['MetaData/Configuration/Antennas/ant1'].attrs['description'] = 'ant1, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, 25.0950 -9.0950 0.0450, , 1.22'
+f['MetaData/Configuration/Antennas/ant2'].attrs['description'] = 'ant2, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, 90.2844 26.3804 -0.22636, , 1.22'
+f['MetaData/Configuration/Antennas/ant3'].attrs['description'] = 'ant3, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, 3.98474 26.8929 0.0004046, , 1.22'
+f['MetaData/Configuration/Antennas/ant4'].attrs['description'] = 'ant4, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, -21.6053 25.4936 0.018615, , 1.22'
+f['MetaData/Configuration/Antennas/ant5'].attrs['description'] = 'ant5, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, -38.2720 -2.5917 0.391362, , 1.22'
+f['MetaData/Configuration/Antennas/ant6'].attrs['description'] = 'ant6, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, -61.5945 -79.6989 0.701598, , 1.22'
+f['MetaData/Configuration/Antennas/ant7'].attrs['description'] = 'ant7, -30:43:17.3, 21:24:38.5, 1038.0, 12.0, -87.9881 75.7543 0.138305, , 1.22'
 
 f.close()
 
 print "Updated antenna positions..."
-
