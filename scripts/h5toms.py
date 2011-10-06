Cross pol delay had incorrect value applied (hv was using vh delay and vice versa)
git-svn-id: https://katfs.kat.ac.za/svnDS/code/katfile/trunk@11345 b56c7a7d-ac47-0410-a905-b6f7de78896a

scripts/h5toms.py
@@ -155,7 +155,7 @@ for scan_ind, compscan_ind, scan_state, target in h5.scans():
             pol_data = []
             uvw_coordinates = np.array(target.uvw(ant2, tstamps, ant1))
             for p in polprods:
-                cable_delay = delays[p[0][-1]][ant2.name] - delays[p[1][-1]][ant1.name]
+                cable_delay = delays[p[1][-1]][ant2.name] - delays[p[0][-1]][ant1.name]
                  # cable delays specific to pol type
                 vis_data = scan_data[:,:,bls.index(list(p))]
                 if options.stop_w: