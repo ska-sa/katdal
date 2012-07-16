(schwardt, simonr) Fixed long-standing bug in frequency calculation where the RF centre frequency was thought to line up with the start of a channel instead of the middle. As it stands, the channel centre frequencies are already lined up with the middle of each channel. For example, in the 1024-channel case channel 0 straddles 0 Hz in the baseband, the non-existent channel 1024 straddles 400 MHz and therefore the middle channel 512 straddles 200 MHz exactly, which is where the RF centre frequency is mapped.
git-svn-id: https://katfs.kat.ac.za/svnDS/code/katfile/trunk@14709 b56c7a7d-ac47-0410-a905-b6f7de78896a

katfile/dataset.py
@@ -119,8 +119,8 @@ class SpectralWindow(object):
         self.num_chans = num_chans
         self.mode = mode if mode is not None else ''
         # Assume that lower-sideband downconversion has been used, which flips frequency axis
-        # Also subtract half a channel width to get frequencies at center of each channel
-        self.channel_freqs = centre_freq - channel_width * (np.arange(num_chans) - num_chans / 2 + 0.5)
+        # Don't subtract half a channel width as channel 0 is centred on 0 Hz in baseband
+        self.channel_freqs = centre_freq - channel_width * (np.arange(num_chans) - num_chans / 2)
 
     def __repr__(self):
         """Short human-friendly string representation of spectral window object."""