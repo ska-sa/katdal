96293d2ab33e0e6aea567d9ed1405c13734d4959
Ludwig Schwardt <ludwig@ska.ac.za>
2017-11-28 15:06:31 +0200

    Rewrite h5toobj to use chunk stores
    This gets rid of a lot of code. It also shows how DictOfArrays can
    directly be applied to an HDF5 file.

--- scripts/h5toobj.py
+++ scripts/h5toobj.py
@@ -33,31 +33,18 @@ import sys
 import time
 import shlex
 import subprocess
-import functools
 from itertools import product
-import io
 
 import numpy as np
 import katdal
+from katdal.chunkstore_rados import RadosChunkStore
+from katdal.chunkstore_s3 import S3ChunkStore
+from katdal.chunkstore import DictOfArraysChunkStore
 import katsdptelstate
 import katsdpservices
 import dask
 import dask.array as da
 from dask.diagnostics import ProgressBar
-
-try:
-    import rados
-except ImportError:
-    # librados (i.e. direct Ceph access) is only really available on Linux
-    rados = None
-
-try:
-    import botocore
-except ImportError:
-    botocore = None
-else:
-    import botocore.config
-    import botocore.session
 
 
 logging.basicConfig()
@@ -95,18 +82,11 @@ def parse_args():
     parser.add_argument('--obj-only', action='store_true',
                         help='Only populate object store - no Redis update')
     args = parser.parse_args()
-    if not rados and not botocore:
-        logger.warning("No Ceph or S3 installation found - storing Redis DB only")
-        args.redis_only = True
     if not args.redis_only:
         use_s3 = args.s3_url is not None
         use_rados = args.ceph_pool is not None
         if use_rados and use_s3:
             parser.error('Please specify either --ceph-pool or --s3-*')
-        if use_rados and not rados:
-            parser.error('Cannot use Ceph as rados Python library is not installed')
-        if use_s3 and not botocore:
-            parser.error('Cannot use S3 as botocore Python library is not installed')
     if args.base_name is None:
         args.base_name = args.file[0].split(".")[0]
     return args
@@ -130,43 +110,6 @@ def redis_bulk_str(r_str, host, port):
     logger.debug("Bulk insert r_str of len %d completed: %s", len(r_str), retout)
 
 
-def open_rados(ceph_conf, ceph_pool):
-    cluster = rados.Rados(conffile=ceph_conf)
-    cluster.connect()
-    available_pools = cluster.list_pools()
-    if ceph_pool not in available_pools:
-        logger.error("Specified pool %s not available in this cluster (%s)",
-                     ceph_pool, available_pools)
-        sys.exit()
-    ioctx = cluster.open_ioctx(ceph_pool)
-    pool_stats = ioctx.get_stats()
-    logger.info("Connected to pool %s. Currently holds %d objects totalling %g GB",
-                ceph_pool, pool_stats['num_objects'],
-                pool_stats['num_bytes'] / 2 ** 30)
-    return ioctx
-
-
-def write_chunk_rados(ioctx, name, chunk):
-    data_fp = io.BytesIO(chunk.data)
-    data_str = data_fp.read()
-    ioctx.write_full(name, data_str)
-
-
-def open_s3(url):
-    session = botocore.session.get_session()
-    config = botocore.config.Config(max_pool_connections=200,
-                                    s3={'addressing_style': 'path'})
-    client = session.create_client(service_name='s3', endpoint_url=url,
-                                   config=config)
-    return client
-
-
-def write_chunk_s3(client, name, chunk):
-    bucket, key = name.split('/', 1)
-    data_fp = io.BytesIO(chunk.data)
-    client.put_object(Bucket=bucket, Key=key, Body=data_fp)
-
-
 def generate_chunks(shape, dtype, target_object_size, dims_to_split=(0, 1)):
     """"""
     dataset_size = np.prod(shape) * dtype.itemsize
@@ -186,21 +129,11 @@ def generate_chunks(shape, dtype, target_object_size, dims_to_split=(0, 1)):
     return tuple(chunks)
 
 
-def dask_from_chunks(chunks, out_name, base_name):
-    single_chunk = all(len(c) == 1 for c in chunks)
-    last_index_per_dim = [np.r_[0, np.cumsum(c)[:-1]][-1] for c in chunks]
-    widths = [len(str(i)) for i in last_index_per_dim]
+def dsk_from_chunks(chunks, out_name):
     keys = list(product([out_name], *[range(len(bds)) for bds in chunks]))
     all_slices = da.core.slices_from_chunks(chunks)
     for key, slices in zip(keys, all_slices):
-        index = [s.start for s in slices]
-        if single_chunk:
-            obj_name = base_name
-        else:
-            index_str = '_'.join(["{:0{width}d}".format(i, width=w)
-                                  for (i, w) in zip(index, widths)])
-            obj_name = base_name + '/' + index_str
-        yield key, slices, obj_name
+        yield key, slices
 
 
 if __name__ == '__main__':
@@ -290,32 +223,35 @@ if __name__ == '__main__':
 
     use_rados = args.ceph_pool is not None
     if use_rados:
-        store = open_rados(args.ceph_conf, args.ceph_pool)
+        obj_store = RadosChunkStore(args.ceph_conf, args.ceph_pool)
+        pool_stats = obj_store.ioctx.get_stats()
+        logger.info("Connected to pool %s. Currently holds %d objects "
+                    "totalling %g GB", args.ceph_pool,
+                    pool_stats['num_objects'], pool_stats['num_bytes'] / 2**30)
         ts_pbs.add("ceph_pool", args.ceph_pool, immutable=True)
         with open(args.ceph_conf, "r") as ceph_conf:
             ts_pbs.add("ceph_conf", ceph_conf.readlines(), immutable=True)
-        save = functools.partial(write_chunk_rados, store)
     else:
-        store = open_s3(args.s3_url)
+        obj_store = S3ChunkStore(args.s3_url)
         ts_pbs.add("s3_endpoint", args.s3_url, immutable=True)
-        save = functools.partial(write_chunk_s3, store)
-    # Convert HDF5 visibilities to complex64 on the fly
-    vis_save = lambda obj, x: save(obj, x.view(np.complex64)[..., 0])
 
     target_object_size = args.obj_size * 2 ** 20
     dask_graph = {}
     schedule = dask.threaded.get
     output_keys = []
-    for dataset in ('timestamps', 'correlator_data', 'weights',
-                    'weights_channel', 'flags'):
-        arr = h5_file['Data'][dataset]
-        dask_graph[dataset] = arr
-        base_name = '/'.join((args.base_name, program_block, stream, dataset))
-        shape = (min(arr.shape[0], max_dumps),) + arr.shape[1:]
+    h5_store = DictOfArraysChunkStore(**h5_file['Data'])
+    for dataset in h5_store.arrays:
+        dataset = str(dataset)
+        arr = h5_store.arrays[dataset]
         dtype = arr.dtype
+        shape = arr.shape
         if dataset == 'correlator_data':
-            shape = shape[:-1]
             dtype = np.dtype(np.complex64)
+            shape = shape[:-1]
+        copy_dataset = 'copy_' + dataset
+        dask_graph[copy_dataset] = arr
+        base_name = obj_store.join(args.base_name, program_block, stream, dataset)
+        shape = (min(shape[0], max_dumps),) + shape[1:]
         chunks = generate_chunks(shape, dtype, target_object_size)
         num_chunks = np.prod([len(c) for c in chunks])
         chunk_size = np.prod([c[0] for c in chunks]) * dtype.itemsize
@@ -323,9 +259,8 @@ if __name__ == '__main__':
                     "~%d bytes each", base_name, shape, num_chunks, chunk_size)
         dask_info = {'dtype': dtype, 'shape': shape, 'chunks': chunks}
         ts_pbs.add(dataset, dask_info, immutable=True)
-        obj_save = vis_save if dataset == 'correlator_data' else save
-        dsk = {k: (obj_save, obj, (da.core.getter, dataset, s))
-               for k, s, obj in dask_from_chunks(chunks, dataset, base_name)}
+        dsk = {k: (obj_store.put, base_name, s, (h5_store.get, dataset, s, dtype))
+               for k, s in dsk_from_chunks(chunks, copy_dataset)}
         dask_graph.update(dsk)
         output_keys.extend(dsk.keys())
     with ProgressBar():
