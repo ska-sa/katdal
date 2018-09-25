Fix computation of weight scaling
It was scaling by standard deviation instead of variance.

katdal/datasources.py
@@ -145,7 +145,7 @@ def _power_scale(block, auto_indices, index1, index2):
     for i in range(block.shape[0]):
         for j in range(block.shape[1]):
             for k in range(len(auto_indices)):
-                auto_scale[k] = np.divide(1, np.sqrt(block[i, j, auto_indices[k]].real))
+                auto_scale[k] = np.divide(1, block[i, j, auto_indices[k]].real)
             for k in range(block.shape[2]):
                 p = auto_scale[index1[k]] * auto_scale[index2[k]]
                 # If either or both of the autocorrelations has zero power then

katdal/test/test_datasources.py
@@ -113,12 +113,12 @@ class TestChunkStoreVisFlagsWeights(object):
 
         # Make up some vis data where the expected scaling factors can be
         # computed by hand. Note: the autocorrs are all set to powers of
-        # 4 so that we avoid any rounding errors.
+        # 2 so that we avoid any rounding errors.
         vis = np.full(shape, 2 + 3j, np.complex64)
-        vis[:, :, index1 == index2] = 4     # Make all autocorrs real
-        vis[3, :, index1 == index2] = 16     # Tests time indexing
-        vis[:, 7, index1 == index2] = 16    # Tests frequency indexing
-        vis[:, :, ants] *= 64               # The (1, 1) baseline
+        vis[:, :, index1 == index2] = 2     # Make all autocorrs real
+        vis[3, :, index1 == index2] = 4     # Tests time indexing
+        vis[:, 7, index1 == index2] = 4     # Tests frequency indexing
+        vis[:, :, ants] *= 8                # The (1, 1) baseline
         vis[4, 5, 0] = 0                    # The (0, 0) baseline
         expected_scale = np.full(shape, 0.25, np.float32)
         expected_scale[3, :, :] = 1 / 16