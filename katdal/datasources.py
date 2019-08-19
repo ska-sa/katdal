33464ca694fd60fed8bf8138da8e6da718220552
Bruce Merry <bmerry@ska.ac.za>
2019-08-19 11:28:26 +0200

    Use dask's literal type instead of _Lost
    Saves us having to reinvent the wheel.

--- katdal/datasources.py
+++ katdal/datasources.py
@@ -30,6 +30,7 @@ import katsdptelstate
 import numpy as np
 import dask.array as da
 from dask.array.rechunk import intersect_chunks
+from dask.core import literal
 import numba
 
 from .sensordata import TelstateSensorData, TelstateToStr
@@ -95,7 +96,7 @@ class VisFlagsWeights(object):
 
 
 def _apply_data_lost(orig_flags, lost, block_id):
-    mark = lost.value.get(block_id)
+    mark = lost.data.get(block_id)
     if not mark:
         return orig_flags    # Common case - no data lost
     flags = orig_flags.copy()
@@ -203,12 +204,6 @@ def weight_power_scale(vis, weights, auto_indices, index1, index2, out=None):
     return out
 
 
-class _Lost(object):
-    """Thin wrapper to prevent dask trying to inspect the underlying dict"""
-    def __init__(self, value):
-        self.value = value
-
-
 class ChunkStoreVisFlagsWeights(VisFlagsWeights):
     """Correlator data stored in a chunk store.
 
@@ -256,8 +251,10 @@ class ChunkStoreVisFlagsWeights(VisFlagsWeights):
                     for piece in pieces:
                         chunk_idx, slices = zip(*piece)
                         lost[chunk_idx].append(slices)
+        # 'literal' is needed to prevent dask copying the dictionary for every
+        # chunk.
         flags = da.map_blocks(_apply_data_lost, darray['flags'], dtype=np.uint8,
-                              name=flags_raw_name, lost=_Lost(lost))
+                              name=flags_raw_name, lost=literal(lost))
         # Combine low-resolution weights and high-resolution weights_channel
         weights = darray['weights'] * darray['weights_channel'][..., np.newaxis]
         # Scale weights according to power
