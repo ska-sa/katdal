7ed190ac03b405eb84742e48357b5e40626178f4
Ludwig Schwardt <ludwig@ska.ac.za>
2020-06-18 13:52:29 +0200

    New 'time_offset' sensor property
    This allows adjustment of the timestamps of any sensor, by
    adding a custom per-sensor time offset.

--- katdal/sensordata.py
+++ katdal/sensordata.py
@@ -746,6 +746,8 @@ class SensorCache(MutableMapping):
         sensor_data = sensor_getter.get()
         # Clean up sensor data if non-empty
         if sensor_data:
+            time_offset = props.get('time_offset', 0.0)
+            sensor_data.timestamp += time_offset
             # Sort sensor events in chronological order and discard duplicates and unreadable sensor values
             sensor_data = remove_duplicates_and_invalid_values(sensor_data)
         if not sensor_data:
