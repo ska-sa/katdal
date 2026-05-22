7abe1571701c9952576877f1b8bc6ca2ad03b825
Ludwig Schwardt <ludwig@ska.ac.za>
2026-05-22 14:58:12 +0200

    SPP1-214 Fix basic package metadata
    I am following the lead of commit 9229a915 of ska-sa/katsdpcalproc.
    
    Move main package metadata from setup.py to the modern pyproject.toml
    (see PEP 621). Fix the license to be an SPDX identifier (see PEP 639),
    but keep the legacy format because we don't yet require setuptools>=77.
    Remove the license trove classifier as recommended.
    
    The README is now dynamic so that it can be concatenated with the NEWS
    by setuptools. Expand the project URLs and install the main user scripts
    via entry points (which is why I moved them inside the main package).
    
    Switch from katversion to setuptools_scm to determine the package
    version dynamically. I'll investigate adding branch names to local
    version strings via setuptools_scm customisation next, which would
    be the perfect send-off for katversion :-D
    
    Pick a version of setuptools_scm (and setuptools) that works with
    Python 3.6 for now. Keep a minimal setup.py for editable installs, which
    can be removed once we depend on setuptools >= 64 (looking at you,
    CAM!).
    
    Delete the unnecessary __init__.py in tests as it is no longer a module.
    Also delete MANIFEST.in as setuptools_scm takes care of that.

--- setup.py
+++ setup.py
@@ -1,74 +1,7 @@
 #!/usr/bin/env python
 
-################################################################################
-# Copyright (c) 2011,2013,2016-2023, National Research Foundation (SARAO)
-#
-# Licensed under the BSD 3-Clause License (the "License"); you may not use
-# this file except in compliance with the License. You may obtain a copy
-# of the License at
-#
-#   https://opensource.org/licenses/BSD-3-Clause
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-################################################################################
+from setuptools import setup
 
-import os.path
-
-from setuptools import find_packages, setup
-
-here = os.path.dirname(__file__)
-readme = open(os.path.join(here, 'README.rst')).read()
-news = open(os.path.join(here, 'NEWS.rst')).read()
-long_description = readme + '\n\n' + news
-
-setup(name='katdal',
-      description='Karoo Array Telescope data access library for interacting '
-                  'with data sets in the MeerKAT Visibility Format (MVF)',
-      long_description=long_description,
-      long_description_content_type='text/x-rst',
-      author='Ludwig Schwardt',
-      author_email='ludwig@ska.ac.za',
-      packages=find_packages(),
-      scripts=[
-          'scripts/mvf_copy.py',
-          'scripts/mvf_download.py',
-          'scripts/mvftoms.py',
-      ],
-      url='https://github.com/ska-sa/katdal',
-      license='Modified BSD',
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: Scientific/Engineering :: Astronomy'],
-      platforms=['OS Independent'],
-      keywords='meerkat ska',
-      python_requires='>=3.6',
-      setup_requires=['katversion'],
-      use_katversion=True,
-      install_requires=[
-          'numpy >= 1.12.0',
-          'katpoint >= 0.9, < 1',
-          'h5py >= 2.3',
-          'numba',
-          'katsdptelstate[rdb] >= 0.10',
-          'dask[array] >= 2.7.0',
-          'requests >= 2.18.0',
-          'pyjwt >= 2',
-          'cityhash >= 0.2.2',
-          'packaging',
-      ],
-      extras_require={
-          'ms': ['python-casacore >= 2.2.1'],
-          's3': [],
-          's3credentials': ['botocore']
-      },
-      tests_require=['cryptography', 'pytest'])
+# The metadata is all in pyproject.toml.
+# This step is just to support editable installs with setuptools < 64.
+setup()
